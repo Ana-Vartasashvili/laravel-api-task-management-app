9e2e06963a5998ec83bef03ffeb006288b844127
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-01-06 19:50:48 +0400

    create a route to add a new task

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,13 @@ Route::get('/tasks', function () {
     return Task::all();
 });
 
+Route::post('/tasks', function () {
+    return Task::create([
+            'title' => "create a laravel project"
+        ]
+    );
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
